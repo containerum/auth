90a13506e2001e1656ab0216293a21b99efe52b7
Vladimir Stolyarov <xakep6666@gmail.com>
2017-10-11 15:39:24 +0300

    Implement main

--- main.go
+++ main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+)
+
+func logExit(err error) {
+	if err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	viper.SetEnvPrefix("ch_auth")
+	viper.AutomaticEnv()
+
+	if err := logLevelSetup(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := logModeSetup(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	httpTracer, err := getTracer(viper.GetString("http_listenaddr"), "ch-auth-rest")
+	logExit(err)
+
+	grpcTracer, err := getTracer(viper.GetString("grpc_listenaddr"), "ch-auth-grpc")
+	logExit(err)
+
+	storage, err := getStorage()
+	logExit(err)
+
+	RunServers(
+		NewHTTPServer(viper.GetString("http_listenaddr"), httpTracer, storage),
+		NewGRPCServer(viper.GetString("grpc_listenaddr"), grpcTracer, storage),
+	)
 }
