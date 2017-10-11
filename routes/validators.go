eae96428f5c9402a6866e33e0dcddda407d41973
Vladimir Stolyarov <xakep6666@gmail.com>
2017-10-11 10:31:01 +0300

    Fix uuid regexp

--- routes/validators.go
+++ routes/validators.go
@@ -9,7 +9,7 @@ import (
 	"bitbucket.org/exonch/ch-grpc/auth"
 )
 
-var uuidRegexp = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`)
+var uuidRegexp = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`)
 
 var standardHeaderValidators = validators{
 	"X-User-ID":         uuidValidator,
